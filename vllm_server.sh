6cc616e9265bb2cd93dea3344c267b34e9d4871b
root <rirv938@gmail.com>
2023-07-29 11:41:45 +0000

    update

---
+++ vllm_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export NCCL_IGNORE_DISABLED_P2P=1
+#python3 -u -m vllm.entrypoints.api_server --model TheBloke/Wizard-Vicuna-13B-Uncensored-HF --tensor-parallel-size 2 --dtype half --swap-space 4
+#python3 -u -m vllm.entrypoints.api_server --model ehartford/Wizard-Vicuna-13B-Uncensored --tensor-parallel-size 1 --dtype half --swap-space 4
+
+pkill -f -9 ray
+pkill -f -9 conda
+sleep 30
+
+#python3 -u -m vllm.entrypoints.api_server --model TheBloke/Wizard-Vicuna-13B-Uncensored-HF --tensor-parallel-size 2 --dtype half --swap-space 4 --quantisation gptq
+python3 -u -m vllm.entrypoints.api_server --model /mnt/pvc/Wizard-Vicuna-13B-Uncensored-GPTQ-4bit-g128 --tensor-parallel-size 2 --dtype half --swap-space 4 --quantisation gptq
