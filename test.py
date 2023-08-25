c1938176cc1005889dc09ae352d7e5efb42950b0
root <rirv938@gmail.com>
2023-08-25 19:58:06 +0100

    update

--- test.py
+++ test.py
@@ -1,17 +1,33 @@
 from tqdm import tqdm
 import os
 import glob
+import mock
 
 from torch.nn import functional as F
 import torch
 
+from vllm.model_executor.layers import quant
 import awq_inference_engine
 import new_inf
+
+from enum import Enum
 
 ROOT = '/code/test_layer'
 
 
-def test_file(f, new_kernel):
+class Target(Enum):
+    original = 1
+    python = 2
+    new = 3
+    python_new_dequant = 4
+
+
+def test_file(f, target_kernel):
+    """
+    We are performing a linear layer on the same
+    quantized data so these results should be very close
+    if not zero.
+    """
     data = torch.load(f)
 
     ins = data['input']
@@ -21,22 +37,24 @@ def test_file(f, new_kernel):
     qweight = data['qweight']
     zeros = data['zeros']
 
-    #if f.endswith('data_842.pt'):
-    #    import pdb; pdb.set_trace()
-
-    if new_kernel:
-        """
-        result = new_inf.gemm_forward_cuda(
-            ins, qweight, scales, zeros, 8
-        )
-        """
-        result = python_impl(
-            ins, qweight, scales, zeros
-        )
-    else:
+    if target_kernel == Target.original:
         result = awq_inference_engine.gemm_forward_cuda(
             ins, qweight, scales, zeros, 8
         )
+    elif target_kernel == Target.python:
+        result = python_impl(
+            ins, qweight, scales, zeros
+        )
+    elif target_kernel == Target.new:
+        result = new_inf.gemm_forward_cuda(
+            ins, qweight, scales, zeros, 8
+        )
+    elif target_kernel == Target.python_new_dequant:
+        result = python_impl_cuda_dequant(
+            ins, qweight, scales, zeros
+        )
+    else:
+        raise NotImplementedError('unknown kernel {}'.format(target_kernel))
 
     assert result.shape == outs.shape
 
@@ -55,14 +73,14 @@ def test_file(f, new_kernel):
     if max_diff.item() > 0.01:
         print('max', max_diff)
         match = False
-    elif max_diff.item() > 0.001:
+    elif max_diff.item() > 0.0001:
         print('max', max_diff)
 
     if mean_diff.item() > 0.0005:
         print('mean', mean_diff)
         match = False
-    elif mean_diff.item() > 0.0005:
-        print('max', max_diff)
+    elif mean_diff.item() > 0.00005:
+        print('mean', mean_diff)
 
     #if new_kernel:
     #import pdb; pdb.set_trace()
@@ -87,7 +105,21 @@ def python_impl(inputs, kernel, scales, zeros):
     return res
 
 
+def python_impl_cuda_dequant(inputs, kernel, scales, zeros):
+    weights = new_inf.dequantize(kernel, scales, zeros)
+    res = F.linear(inputs.to(0), weights.to(0))
+    return res
+
+
 def dequantize_test():
+    """
+    We have the processed kernel, scales, zeros and the original weights
+
+    we cannot recover the original weights from the kernels etc.
+
+    The function f(real weights, scales, zeros) -> kernel is not known
+     and convoluted so cant recover and check this
+    """
     print('running dequantize test')
     path = os.path.join(ROOT, 'dequantize.pt')
     data = torch.load(path)
@@ -97,6 +129,14 @@ def dequantize_test():
     zeros = data['zeros']
     kernel = data['qweight']
 
+    print('checking packing algorithm on kernel')
+    repacked = pack(unpack(kernel))
+    assert (repacked == kernel).all()
+
+    print('checking packing algorithm on zeros')
+    repacked = pack(unpack(zeros))
+    assert (repacked == zeros).all()
+
     """
     dq = new_inf.dequantize(
         kernel,
@@ -105,24 +145,21 @@ def dequantize_test():
     )
     """
 
-    dq = python_dequantize(kernel, scales, zeros)
-
+    # first we check the dequantized weights against real weights
+    # no reason they should be the same
+    dq = python_dequantize(kernel.to(0), scales.to(0), zeros.to(0))
     assert dq.shape == weights.shape
 
-    diff = dq - weights
+    diff = dq - weights.to(0)
     nan_ix = diff.isnan()
 
+    print('dequantized diff with original weights (shouldnt match)')
     print('mean', diff.abs().double().mean())
 
-    """
-    I think this works because some features are
-    super close like almost no error and most do have an error
-    """
-    #assert ((diff == 0.0) | nan_ix).all().item()
     return dq
 
 
-def unpack(matrix, dtype):
+def unpack(matrix, dtype=torch.int32):
     output = torch.zeros((matrix.shape[0], matrix.shape[1] * 8), dtype=dtype)
 
     order_map = [0, 2, 4, 6, 1, 3, 5, 7]
@@ -134,20 +171,25 @@ def unpack(matrix, dtype):
     return output
 
 
+def pack(matrix, dtype=torch.int32):
+    output = torch.zeros((matrix.shape[0], matrix.shape[1] // 8), dtype=dtype)
+
+    pack_num = 32 // 4
+
+    for col in range(matrix.shape[1] // pack_num):
+        order_map = [0, 2, 4, 6, 1, 3, 5, 7]
+        for i in range(pack_num):
+            matrix_col = matrix[:, col * pack_num + order_map[i]]
+            output[:, col] |= matrix_col << (i * 4)
+
+    return output
+
+
 def python_dequantize(kernel, scales, zeros):
     """
-    proof of concept with python
-
-    there is
-    1. scales, zeros as calculated by the algorithm
-    2. qweights, scales and zeros as they are compressed (this is what we have)
-
-    # we need to recover the input weights
-    1. unpad the zeros
-    2. unpad the scales
-    3. unpad the kernel
+    I dont think this works: its a more different function
+    to recover the original weights
     """
-
     in_feats, in_channels = kernel.shape[0], kernel.shape[1] * 8
 
     new_zeros = unpack(zeros, dtype=torch.int32)
@@ -160,23 +202,28 @@ def python_dequantize(kernel, scales, zeros):
 
     new_kernel = unpack(kernel, dtype=torch.int32)
     assert new_kernel.shape == torch.Size([in_feats, in_channels])
+    new_kernel = torch.transpose(new_kernel, 0, 1).to(0)
 
     output = []
-
-    new_kernel = torch.transpose(new_kernel, 0, 1).to(0)
 
     for column in range(in_feats):
         output.append((new_kernel[:, column] * scales[column // 128]) - scale_zeros[column // 128])
 
     res = torch.stack(output, dim=1)
-    return res
+    return res.to(torch.float16)
 
 
 if __name__ == '__main__':
     #dequantize_test()
+    test_cases = [
+        Target.original,
+        Target.python_new_dequant,
+        Target.python,
+        Target.new
+    ]
 
     folders = glob.glob(ROOT + '/regression_*')
-    for new_kernel in [False, True]:
+    for new_kernel in test_cases:
         print('using new kernel: {}'.format(new_kernel))
         for f in folders:
             run_test(f, new_kernel)
