c61a3c7b9d4f34c93b901d0d47d9ed270243550d
robert <robert>
2023-09-01 11:17:24 +0000

    working

--- new_ext/new_kernels/gemm_cuda_gen.cu
+++ new_ext/new_kernels/gemm_cuda_gen.cu
@@ -9,7 +9,7 @@
 #define CEIL_DIV(A, B) (((A) + (B)-1) / (B))
 
 // dimensions of moving block for feats-x, kernel-y and common
-template<int blocksize_x, int blocksize_y, int blocksize_depth>
+template<int blocksize_x, int blocksize_y, int blocksize_depth, int tile_y>
 __global__ void quant_forward_mm(
 	half* in_feats,
        	int* kernel,
@@ -35,84 +35,94 @@ __global__ void quant_forward_mm(
     // one zero and scale per input channel group
     const int groupsize = 128;
 
+    const int num_output_channels = num_packed_channels * 8;
+
     // preload into shared memory which is 10x faster than load from GMEM
     __shared__ float s_weight[blocksize_y * blocksize_depth * 8];
     __shared__ half s_feats[blocksize_x * blocksize_depth];
 
     // need to make sure there is no repeated x-y pairs
     const int x = blockIdx.x * blocksize_x + threadIdx.x / blocksize_depth;
-    const int y = blockIdx.y * blocksize_y + threadIdx.x % blocksize_depth;
 
-    // row and column in block-space
-    const int chunk_row = x / blocksize_x;
-    const int chunk_column = y / blocksize_y;
+    // each tile is responsible for tile_y consecutive elements so this is just the start
+    const int y_offset = blockIdx.y * blocksize_y + (tile_y * threadIdx.x) % blocksize_y;
 
-    // position of the thread inside the moving block in either feats / kernel
-    const int thread_row = threadIdx.x / blocksize_depth;
-    const int thread_column = threadIdx.x % blocksize_depth;
+    // which entries the thread is responsible for in the feat block
+    const int feat_row = threadIdx.x / blocksize_depth;
+    const int feat_col = threadIdx.x % blocksize_depth;
 
-    const int num_output_channels = num_packed_channels * 8;
+    // for the kernel block (in y dimension its the starting position)
+    const int kernel_row = (threadIdx.x * tile_y) / blocksize_y;
+    const int kernel_col_offset = (threadIdx.x * tile_y) % blocksize_y;
 
-    // all the blocks start on column 0
-    half* in_feats_ptr = in_feats + blocksize_x * chunk_row * in_channels;
+    // position the block pointers at the start
+    half* in_feats_ptr = in_feats + blocksize_x * blockIdx.x * in_channels;
+    int* kernel_ptr = kernel + blocksize_y * blockIdx.y;
 
-    // all the blocks start on row 0
-    int* kernel_ptr = kernel + blocksize_y * chunk_column;
+    // one column of kernel becomes 8 in the output due to dequantising
+    // and each thread is responsible for tile-y elements in the kernel
+    float tmp_results[8 * tile_y] = {0.0};
 
-    if (x < num_in_feats && y < num_packed_channels) {
-	// one column of kernel becomes 8 in the output due to dequantising
-        float tmp_results[8] = {0.0};
+    for (int shift = 0; shift < in_channels; shift+=blocksize_depth) {
+       // order not important for correctness but is important for coalescence
+       // relies on assumption that num threads == size of feats block
+       s_feats[feat_row * blocksize_depth + feat_col] = in_feats_ptr[feat_row * in_channels + feat_col];
 
-	for (int shift = 0; shift < in_channels; shift+=blocksize_depth) {
-           // order not important for correctness but is important for coalescence
-           s_feats[thread_row * blocksize_depth + thread_column] = in_feats_ptr[thread_row * in_channels + thread_column];
+       // each thread is responsible for loading tile_y elements from the kernel block
+       for (int tid=0; tid < tile_y; tid++) {
+           int x_kernel_offset = shift + kernel_row;
+           int y_kernel_offset = y_offset + tid;
 
-	   int x_kernel_offset = shift + thread_row;
-           int z_item = *(zeros + x_kernel_offset / groupsize * num_output_channels / 8 + y);
-	   int w_item = kernel_ptr[thread_row * num_packed_channels + thread_column];
+           int z_item = *(zeros + x_kernel_offset / groupsize * num_output_channels / 8 + y_kernel_offset);
+           int w_item = kernel_ptr[kernel_row * num_packed_channels + kernel_col_offset + tid];
 
            // calculate and store the dequantized weights
-	   for (int pos = 0; pos < 8; pos++) {
-	       half s_item = *(scales + x_kernel_offset / groupsize * num_output_channels + y * 8 + order_map[pos]);
+           for (int pos = 0; pos < 8; pos++) {
+               half s_item = *(scales + x_kernel_offset / groupsize * num_output_channels + y_kernel_offset * 8 + order_map[pos]);
 
-	       float zero = static_cast<float>((z_item >> 4 * pos) & 0xf);
-	       float weight = static_cast<float>((w_item >> 4 * pos) & 0xf);
+               float zero = static_cast<float>((z_item >> 4 * pos) & 0xf);
+               float weight = static_cast<float>((w_item >> 4 * pos) & 0xf);
 
-	       float scaled_zero = zero * __half2float(s_item);
-	       float dequant = (weight * __half2float(s_item)) - scaled_zero;
+               float scaled_zero = zero * __half2float(s_item);
+               float dequant = (weight * __half2float(s_item)) - scaled_zero;
 
-	       // index chosen to remove bank conflicts (matrix as pos-row-column order)
-               int idx = order_map[pos] * blocksize_depth * blocksize_y + thread_row * blocksize_depth + thread_column;
-	       s_weight[idx] = dequant;
+               // index chosen to remove bank conflicts (matrix as pos-row-column order)
+               //int idx = order_map[pos] * blocksize_depth * blocksize_y + kernel_row * blocksize_depth + kernel_col_offset + tid;
+               int idx = order_map[pos] * blocksize_depth * blocksize_y + kernel_row * blocksize_y + kernel_col_offset + tid;
+               s_weight[idx] = dequant;
            }
+       }
 
-	   __syncthreads();
+       __syncthreads();
 
-	   // advance the block forward
-	   in_feats_ptr += blocksize_depth;
-	   kernel_ptr += blocksize_depth * num_packed_channels;
+       // advance the block forward
+       in_feats_ptr += blocksize_depth;
+       kernel_ptr += blocksize_depth * num_packed_channels;
 
-	   // multiply the features and weights together
-	   for (int blockpos=0; blockpos < blocksize_depth; blockpos++) {
-	       half f_item = s_feats[thread_row * blocksize_depth + blockpos];
+       // multiply the features and weights together
+       for (int blockpos=0; blockpos < blocksize_depth; blockpos++) {
+           half f_item = s_feats[feat_row * blocksize_depth + blockpos];
 
-	       for (int pos=0; pos < 8; pos++) {
-		   // block cause of high MIO throttling and instructions executed
-		   float dequant = s_weight[pos * blocksize_depth * blocksize_y + blockpos * blocksize_depth + thread_column];
+           for (int tid=0; tid < tile_y; tid++) {
+               for (int pos=0; pos < 8; pos++) {
+	           //float dequant = s_weight[pos * blocksize_depth * blocksize_y + blockpos * blocksize_depth + kernel_col_offset + tid];
+	           float dequant = s_weight[pos * blocksize_depth * blocksize_y + blockpos * blocksize_y + kernel_col_offset + tid];
 	           float value = __half2float(f_item) * dequant;
-	           tmp_results[pos] +=  value;
-	       }
+	           tmp_results[tid * 8 + pos] +=  value;
+               }
            }
+       }
 
-	   // next loop will change the shared memory again
-	   __syncthreads();
-        }
+       // next loop will change the shared memory again
+       __syncthreads();
+    }
 
-	// write out the results for this position
-        for (int pos=0; pos < 8; pos++) {
-	    half* out_ptr = out_feats + x * num_output_channels + y * 8 + pos;
-	    *(half*)(out_ptr) = __float2half(tmp_results[pos]);
-        }
+    // write out the results for this position
+    for (int pos=0; pos < 8; pos++) {
+	for (int tid=0; tid < tile_y; tid++) {
+            half* out_ptr = out_feats + x * num_output_channels + (y_offset + tid) * 8 + pos;
+            *(half*)(out_ptr) = __float2half(tmp_results[tid * 8 + pos]);
+	}
     }
 
 }
@@ -168,21 +178,29 @@ torch::Tensor gemm_forward_cuda_new(
         throw std::invalid_argument("Packed channels must be divisible by 8");
     }
 
-    // for now they must be the same dimension
-    const int blocksize_y = 8;
+    const int blocksize_y = 64;
     const int blocksize_x = 8;
     const int blocksize_depth = 8;
+    const int tiles_y = 8;
 
-    assert(blocksize_x == blocksize_y);
     assert(num_in_feats % blocksize_x == 0);
     assert(num_in_channels % blocksize_depth == 0);
     assert(num_packed_channels % blocksize_y == 0);
 
-    dim3 threads(blocksize_depth * blocksize_x);
+    // each thread will be responsible for tiles_y channels in the y-dimension
+    const int num_threads = blocksize_x * blocksize_y / tiles_y;
+    dim3 threads(num_threads);
+
+    // check the shapes compatible
+    assert(blocksize_y % tiles_y == 0);
+
+    // check that threads can load the data as expected
+    assert(num_threads == blocksize_x * blocksize_depth);
+    assert(num_threads == blocksize_y * blocksize_depth / tiles_y);
 
     dim3 blocks(CEIL_DIV(num_in_feats, blocksize_x), CEIL_DIV(num_packed_channels, blocksize_y));
 
-    quant_forward_mm<blocksize_x, blocksize_y, blocksize_depth><<<blocks, threads>>>(
+    quant_forward_mm<blocksize_x, blocksize_y, blocksize_depth, tiles_y><<<blocks, threads>>>(
         in_feats, kernel, scaling_factors, zeros, out_feats, num_in_channels,
        	num_packed_channels, num_in_feats
     );
